04b40e9979138e58e87c227d3dc2c9fcb3b3e15c
Guilherme Almeida <guilherme.dev.almeida@gmail.com>
2023-03-17 17:38:23 -0300

    Correção das pags

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const vandalCarrousel = document.querySelector('.vandal--carrousel')
 const btnPrev = document.querySelector('.btn-prev')
 const btnNext = document.querySelector('.btn-next')
+const vandalThumb = document.querySelector('.vandal--thumb')
+const videoHighlight = document.querySelector('.video--highlight')
+
 
 let skins = []
 let page = 0
 
 const MIN_PAGE = 0
-const MAX_PAGE = 43
+const MAX_PAGE = 45
 
 btnPrev.addEventListener('click', () => {
-    if (page === 0) {
+    if (page <= 0 || page <= 3) {
         page = MAX_PAGE;
         filterVandal()
-
         return
     }
-
     page -= 5;
-
     filterVandal()
 });
 
@@ -38,12 +38,14 @@ function filterVandal() {
 
     for (let i = page; i < page + 5; i++) {
         const vandal = vandalsData[i];
+        console.log(page);
 
         const vandalThumb = document.createElement('div')
         vandalThumb.classList.add('vandal--thumb')
         vandalThumb.style.backgroundImage = `url(${vandal.displayIcon})`
         vandalCarrousel.appendChild(vandalThumb);
     }
+
 }
 
 
