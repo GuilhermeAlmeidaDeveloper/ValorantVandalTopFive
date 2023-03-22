26da90524cd40803cf00e8b1a772e1e92bc3358c
Guilherme Almeida <guilherme.dev.almeida@gmail.com>
2023-03-22 01:32:22 -0300

    feat: exposição do vídeo + disponibilização das skins
    feat: exposição do vídeo + disponibilização das skins

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const vandalCarrousel = document.querySelector('.vandal--carrousel')
 const btnPrev = document.querySelector('.btn-prev')
 const btnNext = document.querySelector('.btn-next')
-const vandalThumb = document.querySelector('.vandal--thumb')
 const videoHighlight = document.querySelector('.video--highlight')
-
+const availableChromas = document.querySelector('.available-chromas')
+const skinDisponivel = document.querySelector('.vandal-color-available')
+const vandalVideo = document.querySelector('.vandal--video')
 
 let skins = []
+let skin = null
 let page = 0
 
 const MIN_PAGE = 0
@@ -43,8 +45,41 @@ function filterVandal() {
         const vandalThumb = document.createElement('div')
         vandalThumb.classList.add('vandal--thumb')
         vandalThumb.style.backgroundImage = `url(${vandal.displayIcon})`
+        vandalThumb.onclick = function () {
+            skin = vandal
+
+            availableChromas.innerHTML = ''
+            vandalVideo.innerHTML = ''
+
+            const skinVideo = document.createElement('iframe')
+            skinVideo.setAttribute('src', skin.levels[skin.levels.length - 1].streamedVideo)
+            skinVideo.setAttribute('width', '615px')
+            skinVideo.setAttribute('height', '400px')
+            skinVideo.setAttribute('allow', 'autoplay')
+            skinVideo.setAttribute('frameborder', '0')
+
+            skinVideo.muted = true;
+
+            vandalVideo.appendChild(skinVideo)
+
+            for (const chroma of skin.chromas) {
+                const skinDisponivel = document.createElement('div')
+                skinDisponivel.classList.add('vandal-color-available')
+                skinDisponivel.style.backgroundImage = `url(${chroma.displayIcon || chroma.fullRender || skin.displayIcon})`
+                skinDisponivel.onclick = function () {
+                    console.log(chroma)
+                }
+
+                availableChromas.appendChild(skinDisponivel)
+            }
+
+
+        };
         vandalCarrousel.appendChild(vandalThumb);
     }
+
+
+
 
 }
 
@@ -58,6 +93,8 @@ async function loadSkins() {
 }
 
 loadSkins()
+
+
 
 
 
